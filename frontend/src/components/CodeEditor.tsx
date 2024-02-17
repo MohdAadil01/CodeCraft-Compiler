0af9674b5083c71004d9166c4fd8959f153796bb
MohdAadil01 <mohdaadil4240@gmail.com>
2024-02-17 16:01:16 +0530

    deploy

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { tags as t } from "@lezer/highlight";
 import { draculaInit } from "@uiw/codemirror-theme-dracula";
